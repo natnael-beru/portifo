5a35d4ec609755ba0ece237658bbb61c00987672
natnael-beru <120000938+natnael-beru@users.noreply.github.com>
2023-01-03 13:21:58 -0600

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -22,7 +22,7 @@ const SocialLinks = () => {
           GitHub <FaGithub size={30} />
         </>
       ),
-      href: "https://github.com/theyashpatel",
+      href: "https://github.com/natnael-beru",
     },
     {
       id: 3,
@@ -31,7 +31,7 @@ const SocialLinks = () => {
           Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: "mailto:foo@gmail.com",
+      href: "natisha@gmail.com",
     },
     {
       id: 4,
